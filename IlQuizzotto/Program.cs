b41b1feca240c49fe917b6f273e24083436fece9
Nicola Andrenucci <n.andrenucci@xeel.tech>
2024-12-13 14:40:37 +0100

    signalr

--- IlQuizzotto/Program.cs
+++ IlQuizzotto/Program.cs
@@ -1,8 +1,10 @@
+using IlQuizzotto.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-
+builder.Services.AddSignalR();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,5 +23,5 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
-
+app.MapHub<ChatHub>("/quizzottoHub");
 app.Run();
