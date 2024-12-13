656b9d3397bd8183dc7a5d0e3c3b88c5e4bdbd8b
Nicola Andrenucci <n.andrenucci@xeel.tech>
2024-12-13 16:43:42 +0100

    layout for question

--- IlQuizzotto/Pages/ActiveQuestion.cshtml.cs
+++ IlQuizzotto/Pages/ActiveQuestion.cshtml.cs
@@ -1,8 +1,10 @@
 using IlQuizzotto.Hubs;
+using IlQuizzotto.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.SignalR;
+using System.Linq;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace IlQuizzotto.Pages
@@ -11,20 +13,32 @@ namespace IlQuizzotto.Pages
     {
         IHubContext<QuizzottoHub> _hub;
         public string currentQuestion = string.Empty;
-
-        public ActiveQuestionModel(IHubContext<QuizzottoHub> hub)
+        public string answer1 = string.Empty;
+        public string answer2 = string.Empty;
+        public string answer3 = string.Empty;
+        public ApplicationDbContext _context;
+        public ActiveQuestionModel(IHubContext<QuizzottoHub> hub, ApplicationDbContext context)
         {
             _hub = hub;
-
+            _context = context;
         }
-        public void OnGet()
+        public async void OnGet()
         {
+            
             if (HttpContext.Session.GetInt32("currentQuestion") == null)
             {
                 HttpContext.Session.SetInt32("currentQuestion", 0);
             }
-
-            currentQuestion = "Domanda " + HttpContext.Session.GetInt32("currentQuestion");
+            var q = _context.Set<Question>().FirstOrDefault();
+            var answers = _context.Set<Answer>().Where(c=>c.QuestionId == q.Id).ToArray();
+            currentQuestion = q.Description;
+            await _hub.Clients.All.SendAsync("CurrentQuestion", currentQuestion);
+            await _hub.Clients.All.SendAsync("Answer1", "Risposta1 " + answers[0].Description);
+            answer1 = answers[0].Description;
+            await _hub.Clients.All.SendAsync("Answer2", "Risposta2 " + answers[1].Description);
+            answer2 = answers[1].Description;
+            await _hub.Clients.All.SendAsync("Answer3", "Risposta3 " + answers[2].Description);
+            answer3 = answers[2].Description;
         }
         public async Task OnPostAsync()
         {
@@ -33,10 +47,13 @@ namespace IlQuizzotto.Pages
             currentQuestion = "Domanda " + currentQuestioNumber;
             HttpContext.Session.SetInt32("currentQuestion", currentQuestioNumber ?? 0);
 
+            var q = _context.Set<Question>().Skip(currentQuestioNumber.Value).FirstOrDefault();
+            var answers = _context.Set<Answer>().Where(c => c.QuestionId == q.Id).ToArray();
+            currentQuestion = q.Description;
             await _hub.Clients.All.SendAsync("CurrentQuestion", currentQuestion);
-            await _hub.Clients.All.SendAsync("Answer1", "Risposta1 " + currentQuestioNumber);
-            await _hub.Clients.All.SendAsync("Answer2", "Risposta2 " + currentQuestioNumber);
-            await _hub.Clients.All.SendAsync("Answer3", "Risposta3 " + currentQuestioNumber);
+            await _hub.Clients.All.SendAsync("Answer1", "Risposta1 " + answers[0].Description);
+            await _hub.Clients.All.SendAsync("Answer2", "Risposta2 " + answers[1].Description);
+            await _hub.Clients.All.SendAsync("Answer3", "Risposta3 " + answers[2].Description);
 
         }
         public void CollectAnswer(string userName, int answer)
